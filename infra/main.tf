51cab1a6241ad2a51bd30a408cab8e35e81d990e
MUNEESH08 <220701175@rajalakshmi.edu.in>
2025-10-29 23:08:06 +0530

    Refactor Vercel project and deployment resources

--- infra/main.tf
+++ infra/main.tf
@@ -11,7 +11,6 @@ provider "vercel" {
   api_token = var.vercel_api_token
 }
 
-# Create a Vercel project linked to your GitHub repo
 resource "vercel_project" "terra_web_app" {
   name = "terra-web-app"
 
@@ -22,16 +21,16 @@ resource "vercel_project" "terra_web_app" {
   }
 }
 
-# Trigger a deployment (for Docker-based project)
 resource "vercel_deployment" "terra_web_app_deploy" {
   project_id = vercel_project.terra_web_app.id
-  files = [
-    {
-      path = "Dockerfile"
+
+  files = {
+    "Dockerfile" = {
       sha  = filemd5("${path.module}/../Dockerfile")
       data = file("${path.module}/../Dockerfile")
     }
-  ]
+  }
+
   production = true
 }
 
